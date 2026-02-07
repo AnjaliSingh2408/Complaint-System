859e9326b6ececed23ea033af877795f18706233
Anjali Singh <anjalikashi08@gmail.com>
2026-02-07 20:05:18 +0530

    Implement refresh token functionality and logout endpoint

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,10 +3,14 @@ import { Router } from "express"
 import { upload } from "../middlewares/multer.middlerwares.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js" 
 import { registerCitizen,
-    loginUser
+    loginUser,
+    refreshAccessToken,
+    logoutUser
  } from "../controllers/auth.controllers.js"
 
 const router = Router()
 
 router.route("/register").post(registerCitizen)
 router.route("/login").post(loginUser)
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
